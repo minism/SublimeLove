0c09d375e985e9c0c070022c9d4e254fba7401fa
Jan Kasiak <j.kasiak@gmail.com>
2013-05-23 14:55:25 -0400

    Improve GLSL syntax highlighting, and function completion
    * Scrape Love2D wiki for API functions and arguments
    * Add arguments to completion for non-overloaded functions
    * GLSL syntax highlighting for newPixelEffect
    * Build using JSON-tmLanguage file

--- README.md
+++ README.md
@@ -19,9 +19,13 @@ Syntax highlighting
 -------------------
 Hit ctrl/cmd+P, or go to View > Syntax, and set the syntax of your open file to "Lua (Love)" to enable highlighting for the Love2D API functions.  On my editor, lua files are now defaulting to this syntax, but I'm not sure how to control that.
 
+If you create a new pixel effect, and make the argument a multi-line string, then Sublime will use C syntax highlighting for the GLSL content.
+
 Auto completion
 ---------------
 Pressing ctrl+space in an open Love file will show the autocompletions for the API functions.  ST2 currently has some issues with autocomplete that other plugins are also dealing with, so its not perfect yet.  One major issue is that the period key breaks tokens and doesn't get included as part of the autocomplete query.
+
+Those Love2D functions which are not overloaded (only one possible argument combination), will fill in the argument names for you.
 
 Build system
 ------------
@@ -31,3 +35,9 @@ The build system will automatically be selected for lua files.
 
 Links
 =====
+
+Development
+===========
+Go to the **src** directory. If Love2D has been updated, run **scrape_api.py**, which generates **api.txt** and **callbacks.txt**, then run **build.py** which assembles the Sublime files.
+
+**Love.JSON-tmLanguage** is an expanded version of the Lua syntax highlighter. Leave the **lua-love** repository entry empty, as its filled in by **build.py**
--- src/build.py
+++ src/build.py
@@ -3,17 +3,14 @@
 import sys
 import os
 import plistlib
-import json
+import simplejson
 
-lua_syntax = os.path.join('..', '..', 'Lua', 'Lua.tmLanguage')
+lua_syntax = os.path.join('..', 'Love.JSON-tmLanguage')
 syntax_out = os.path.join('..', 'Love.tmLanguage')
 completions_out = os.path.join('..', 'Love.sublime-completions')
-
-name = 'Lua (Love)'
-comment = 'Lua+Love Syntax'
 scopeName = 'source.lua.love'
-uuid = '86A2712F-D1D2-4AB3-8788-B4A6B6215005'
 syntaxGroup = 'support.function.library.lua.love'
+
 
 def tmize(node):
     assert(isinstance(node, dict))
@@ -24,50 +21,55 @@ def tmize(node):
         else:
             buf.append('%s\.%s' % (key, tmize(val)))
     return '(%s)' % '|'.join(buf)
- 
+
+
 def main():
     # Parse api
     fh = open('api.txt')
-    api_list = [line.strip() for line in fh.read().split() if line]
+    api_list = [line.strip().split(',') for line in fh.read().split() if line]
     fh.close()
     fh = open('callbacks.txt')
-    callback_list = [line.strip() for line in fh.read().split() if line]
+    callback_list = [line.strip().split(',') for line in fh.read().split() if line]
     fh.close()
 
     # Build api hierarchy
     api_dict = {}
     for line in api_list:
         namespace = api_dict
-        for token in line.split('.'):
+        api_call = line[0]
+        for token in api_call.split('.'):
             if not namespace.get(token):
                 namespace[token] = {}
             namespace = namespace[token]
-    # Clone lua syntax and insert metadata
-    plist = plistlib.readPlist(lua_syntax)
-    plist['name'] = name
-    plist['scopeName'] = scopeName
-    plist['comment'] = comment
-    plist['uuid'] = uuid
+    # Clone lua syntax
+    lua = simplejson.load(open(lua_syntax, 'r'))
 
     # Insert love API
-    lovedict = {
-        'name': syntaxGroup,
-        'match': r'(?<![^.]\.|:)\b%s\b(?=[( {])' % tmize(api_dict)
-    }
-    plist['patterns'].append(lovedict)
-    plistlib.writePlist(plist, syntax_out)
+    love_match = r'(?<![^.]\.|:)\b%s\b(?=[( {])' % tmize(api_dict)
+    lua['repository']['lua-love']["match"] = love_match
+    plistlib.writePlist(lua, syntax_out)
     print 'Created %s' % syntax_out
 
-    # Build completions file
-
-    completions = []
+    # Filter unique arguments
+    api_args = {}
     for line in api_list + callback_list:
+        api_call = line[0]
+        if api_call not in api_args:
+            api_args[api_call] = line[1:]
+        else:
+            # We don't know how to nicely handle overloading
+            api_args[api_call] = []
+
+    # Build completions file
+    completions = []
+    for call, args in api_args.iteritems():
+        arguments = ["${%s:%s}" % (i + 1, arg) for (i, arg) in enumerate(args)]
         completions.append({
-            'trigger': line,
-            'contents': '%s($0)' % line,
+            'trigger': call,
+            'contents': '%s(%s)' % (call, ', '.join(arguments) if len(arguments) != 0 else "$0"),
         })
     fh = open(completions_out, 'w')
-    json.dump({
+    simplejson.dump({
         'scope': scopeName,
         'completions': completions,
     }, fh, sort_keys=True, indent=4)
@@ -75,11 +77,4 @@ def main():
     print 'Created %s' % completions_out
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        lua_syntax = sys.argv[1]
-
-    if not os.path.exists(lua_syntax):
-        print 'Lua.tmLanguage not found.  Try passing it as an argument'
-        sys.exit(1)
-
     main()
